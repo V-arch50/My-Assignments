80bcaab2bacb0cc65c618cc65b4462c56c6e6760
Varun Singh <62279166+V-arch50@users.noreply.github.com>
2026-03-29 11:46:26 +0530

    Add files via upload

---
+++ Firefox.java
@@ -0,0 +1,18 @@
+package week1;
+
+public class Firefox {
+
+    public static void main(String[] args) {
+        float browserversion= 100.2f;
+        String browsername = "firefox";
+        boolean isVisible = true;
+        int releaseyear = 1998;
+        char browserlogo = 'f';
+        System.out.println("BrowserVersion = "+browserversion+"\n"
+           + "Browser Name = "+browsername+"\n"+"Visibility = "+isVisible+"\n"
+           +"Release Year = "+releaseyear+"\n"+"Browser Logo = "+browserlogo
+        );
+
+    }
+
+}
